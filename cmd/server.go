91641360fc8bf41cb89983115e066f07dcc7a2d4
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2026-01-16 22:38:39 +0000

    implement multi-target exporter pattern

--- cmd/server.go
+++ cmd/server.go
@@ -19,35 +19,36 @@ var ServerCmd = &cobra.Command{
 	Run: GetRunFn(func(cmd *cobra.Command, args []string) error {
 		logger := log.MustLogger(cmd.Context())
 
-		hubAddr, err := cmd.Flags().GetString("address")
-		if err != nil {
-			return err
-		}
 		port, err := cmd.Flags().GetInt("port")
 		if err != nil {
 			return err
 		}
 
-		hubExporter := exporter.NewHubExporter(hubAddr, 5*time.Second)
-
-		registry := prometheus.NewRegistry()
-		if err := registry.Register(hubExporter); err != nil {
-			return err
-		}
-
-		handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
-
+		// /probe implements the multi-target exporter pattern. It expects a GET
+		// parameter "target" containing the address of the Hub to probe.
 		mux := http.NewServeMux()
-		mux.Handle("/metrics", handler)
+		mux.HandleFunc("/probe", func(w http.ResponseWriter, r *http.Request) {
+			target := r.URL.Query().Get("target")
+			if target == "" {
+				http.Error(w, "missing 'target' parameter", http.StatusBadRequest)
+				return
+			}
+
+			registry := prometheus.NewRegistry()
+			hubExporter := exporter.NewHubExporter(target, 5*time.Second)
+			registry.MustRegister(hubExporter)
+
+			handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+			handler.ServeHTTP(w, r)
+		})
 
 		listen := fmt.Sprintf(":%d", port)
-		logger.Info("Starting server", "listen", listen, "hub_address", hubAddr)
+		logger.Info("Starting server", "listen", listen)
 		return http.ListenAndServe(listen, mux)
 	}),
 }
 
 func init() {
-	ServerCmd.Flags().String("address", "192.168.100.1", "Address of VirginMedia Hub 6")
 	ServerCmd.Flags().Int("port", 9188, "HTTP listen port for the exporter")
 
 	RootCmd.AddCommand(ServerCmd)
